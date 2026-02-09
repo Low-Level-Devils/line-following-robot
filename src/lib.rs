9a706389e1ead19895e1dd6435b8fd205e5a1a9e
Josh Walker <walker11710@gmail.com>
2026-02-08 20:32:56 -0700

    add logic to tcrt5000 and l298n tasks

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,8 @@ pub mod line_following_robot {
     use log::info;
     use tcrt5000_driver::tcrt5000::{self, Tcrt5000};
 
-    static MOTOR_COMMAND_CHANNEL: Channel<CriticalSectionRawMutex, MotorCommand, 2> =
+    static MOTOR_COMMAND_CHANNEL: Channel<CriticalSectionRawMutex, Direction, 2> =
         Channel::new();
-
-    #[derive(Clone, Copy)]
-    pub struct MotorCommand {
-        pub direction: Direction,
-    }
 
     pub struct Tcrt5000ArrayInitStruct {
         pub left_pin: AnyPin<'static>,
@@ -36,9 +31,9 @@ pub mod line_following_robot {
 
     #[derive(Clone, Copy)]
     pub enum Direction {
-        Straight,
-        SlightLeft,
         FullLeft,
+        SlightLeft,
+        Straight,
         SlightRight,
         FullRight,
     }
@@ -62,8 +57,29 @@ pub mod line_following_robot {
         let tcrt_right_pin = tcrt5000::initialize_tcrt5000(tcrt5000_array_init_struct.right_pin);
         let tcrt_right = Tcrt5000::new(tcrt_right_pin, true);
 
+        let trct5000_array = [tcrt_left, tcrt_mid_left, tcrt_center, tcrt_mid_right, tcrt_right];
+
+        info!("TCRT5000 Array initialized. Begin polling...");
+
         loop {
-            //TODO Implement polling logic
+            for (index, sensor) in trct5000_array.iter().enumerate()  {
+                if sensor.poll_sensor() {
+                    let pos = match index {
+                        0 => Direction::FullLeft,
+                        1 => Direction::SlightLeft,
+                        2 => Direction::Straight,
+                        3 => Direction::SlightRight,
+                        4 => Direction::FullRight,
+                        _ => continue,
+                    };
+
+                    MOTOR_COMMAND_CHANNEL.send(pos).await;
+
+                    break;
+                }
+            }
+
+            Timer::after(Duration::from_millis(1)).await;
         }
     }
 
@@ -90,8 +106,36 @@ pub mod line_following_robot {
             init_struct.right_cathode,
         );
 
+        l298n_module_left.change_speed(50);
+        l298n_module_right.change_speed(50);
+
+        info!("l298n modules initialized! Searching for path");
+
         loop {
-            //TODO implement motor control logic
+            let command = MOTOR_COMMAND_CHANNEL.receive().await;
+
+            match command {
+                Direction::FullLeft => {
+                    l298n_module_left.change_speed(75);
+                    l298n_module_right.change_speed(0);
+                }
+                Direction::SlightLeft => {
+                    l298n_module_left.change_speed(75);
+                    l298n_module_right.change_speed(25);
+                }
+                Direction::Straight => {
+                    l298n_module_left.change_speed(50);
+                    l298n_module_right.change_speed(50);
+                }
+                Direction::SlightRight => {
+                    l298n_module_left.change_speed(25);
+                    l298n_module_right.change_speed(75);
+                }
+                Direction::FullRight => {
+                    l298n_module_left.change_speed(0);
+                    l298n_module_right.change_speed(75);
+                }
+            }
         }
     }
 }
